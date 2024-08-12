ac0e8a2bd1d7a6de6f754c5bc59259d3189f80c2
teddio496 <teddio496@gmail.com>
2024-08-12 03:49:01 -0400

    minor fix

--- UofTCalculator/UofTCalculator/settings.py
+++ UofTCalculator/UofTCalculator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-zgi23l7=+i-kt*3*e3rtlyf=u66g#2+*01wm_hatfy^r6l7%ry
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Calculator',
-    'vercel',
 ]
 
 MIDDLEWARE = [
